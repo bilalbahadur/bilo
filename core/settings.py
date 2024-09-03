8d1d062b37754cf91bcdfb3fd191db73eac0cf47
bilalbahadur <104194235+bilalbahadur@users.noreply.github.com>
2024-09-03 21:57:38 +0300

    settings.py hatalarla dolu

--- core/settings.py
+++ core/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,17 +117,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL= '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
